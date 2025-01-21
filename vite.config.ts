088ea788543c3794b915a7ae3534768a42b778f1
Jordan Walker <walker.jlg@gmail.com>
2025-01-21 08:56:40 +0000

    Update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import trim from 'lodash-es/trim'
+
+const getBasePath = () => {
+  if (!process.env.BASE_URL) {
+    return '/'
+  }
+
+  return `/${trim(process.env.BASE_URL, '/')}/`
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: getBasePath(),
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
